5db65e5046e8fd9698319b9736b141f398b49818
kjeganathan <68821572+kjeganathan@users.noreply.github.com>
2021-11-04 17:58:12 -0400

    added express version of create account to the server

---
+++ server/expressindex.js
@@ -0,0 +1,33 @@
+'use strict';
+const express = require('express');
+let http = require('http');
+let url = require('url');
+let fs = require('fs');
+const app = express();
+
+app.use(express.json()); // lets you handle JSON input
+
+const port = 3000;
+
+let data = {};
+const filename = 'data.json';
+data = JSON.parse(fs.readFileSync(filename));
+// curl -d '{ "email" : "x", "password" : "X", "firstName" : "x", "lastName" : "x", "userId" : "7", "groups" : ["Esports club"], "previousBookings" : [1], "upcomingBookings" : [2]}' -H "Content-Type: application/json" http://localhost:3000/createAccount
+app.post('/createAccount', (req, res) => {
+    const body = req.body;
+    const first = req.body['firstName'];
+    const last = req.body['lastName'];
+    const id = req.body['userId'];
+    data["users"].push(body);
+    let strInput = JSON.stringify(data);
+    fs.writeFileSync(filename, strInput);
+    res.send(`Created new account for ${first} ${last} with id number ${id}`);
+});
+
+app.get('*', (req, res) => {
+    res.send('NO FOOL, BAD COMMAND');
+  });
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
