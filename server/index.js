777fba0828ae8bfde0a97da48c8ca587849592d9
kjeganathan <68821572+kjeganathan@users.noreply.github.com>
2021-11-03 21:25:20 -0400

    added create account end point

--- server/index.js
+++ server/index.js
@@ -6,12 +6,28 @@ let fs = require('fs');
 const filename = 'data.json';
 let data = {};
 
+
+function createAccount(response, options){
+    data["users"].push(options);
+    response.end();
+    let strInput = JSON.stringify(data);
+    fs.writeFileSync(filename, strInput);
+}
+
 function serverFunction(request, response, options){
     const headerText = {'content-type': 'text/json'};
     response.writeHead(200, headerText);
-    
+
+    if(request.url.startsWith("/createAccount")){
+        createAccount(response, options); //creates a new JSON object if a person does not exist
+        return;
+    }
+    response.end();
+
+
 }
 
+data = JSON.parse(fs.readFileSync(filename));
 let server = http.createServer();
 server.on('request', async (request, response) => {
  
