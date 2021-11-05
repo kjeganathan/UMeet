427275b6fa0bfae5995806de78b1f177df9c2998
kjeganathan <68821572+kjeganathan@users.noreply.github.com>
2021-11-05 17:25:31 -0400

    added the get request for userInfo

--- server/expressindex.js
+++ server/expressindex.js
@@ -12,6 +12,24 @@ const port = 3000;
 let data = {};
 const filename = 'data.json';
 data = JSON.parse(fs.readFileSync(filename));
+
+//browser url localhost:3000/login
+app.get('/login', (req, res) => {
+    res.send(`Login succeeded with status code ${res.statusCode}`);
+});
+
+// browser url localhost:3000/userInfo?userId=1
+app.get('/userInfo', (req, res) => {
+    const k = req.query["userId"];
+    for(let i = 0 ; i < data["users"].length; ++i){
+        if(k === JSON.stringify(data["users"][i].userId)){
+            console.log(data["users"][i]);
+            res.send(data["users"][i]);
+        }
+    }
+    res.send();
+});
+
 // curl -d '{ "email" : "x", "password" : "X", "firstName" : "x", "lastName" : "x", "userId" : "7", "groups" : ["Esports club"], "previousBookings" : [1], "upcomingBookings" : [2]}' -H "Content-Type: application/json" http://localhost:3000/createAccount
 app.post('/createAccount', (req, res) => {
     const body = req.body;
