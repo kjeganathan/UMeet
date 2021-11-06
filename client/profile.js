07c94b8100dadcc5cdbde064d698598417aff4e1
kjeganathan <68821572+kjeganathan@users.noreply.github.com>
2021-11-06 16:55:57 -0400

    added logout reroute

--- client/profile.js
+++ client/profile.js
@@ -49,3 +49,8 @@ window.onclick = function(event) {
   }
 }
 
+let logoutButton = document.getElementById('logOut');
+logoutButton.addEventListener('click', () => {
+    document.location.href = "http://localhost:3000/";
+});
+
