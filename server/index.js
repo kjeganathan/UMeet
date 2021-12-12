217d58e4b6a53a4f1fff92fc7864b17c63c4d689
Disha Srivastava <disha.srivastava07@gmail.com>
2021-12-11 20:53:07 -0500

    Update authentication

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 'use strict';
-
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 require('dotenv').config();
 
@@ -8,48 +7,46 @@ const app = express();
 let http = require('http');
 let fs = require('fs');
 const path = require('path');
-const passport = require('passport');               // handles authentication
+const passport = require('passport'); // handles authentication
 const expressSession = require('express-session');  // for managing session state 
-const LocalStrategy = require('passport-local').Strategy; // username/password strategy
+const passportLocal = require('passport-local'); // username/password strategy
 const dblast = require("./database.js");
-const bcrypt = require('bcrypt');
-const flash = require("connect-flash");
-
-// session configuration
-const session = {
-    secret: process.env.SECRET || 'SECRET', // set this encryption key in Heroku config (never in GitHub)!
-    resave: false,
-    saveUninitialized: false
-};
+const minicrypt = require('./miniCrypt');
+const mc = new minicrypt();
 
 // passport configuration
-const strategy = new LocalStrategy(
-    async (username, password, done) => {
-        if (!findUser(username)) {
-            // no such user
-            return done(null, false, { 'message': 'Wrong username' });
+const LocalStrat = passportLocal.Strategy;
+const session = {
+     secret: process.env.SECRET || 'SECRET', // set this encryption key in Heroku config (never in GitHub)!
+     resave: false,
+     saveUninitialized: false
+ };
+
+ const getSaltHashOf = async (email) => {
+    try {
+        let user = await dblast.getUserByEmail(email);
+        const passwordString = user[0]["password"];
+        return passwordString.password.split(','); // returns an array with element 0 as the the salt and element 1 as the hash
+    } catch(e) {
+        return undefined;
+    }
+};
+
+const strategy = new LocalStrat({usernameField: 'email', passwordField: 'password'},
+    async (email, pass, done) => {
+        if (await getSaltHashOf(email) === undefined) {
+            return done(null, false, {'message' : 'No user with that email exists'});
         }
-        if (!validatePassword(username, password)) {
-            // invalid password
-            // should disable logins after N messages
-            // delay return to rate-limit brute-force attacks
-            await new Promise((r) => setTimeout(r, 2000)); // two second delay
-            return done(null, false, { 'message': 'Wrong password' });
+        if (!(await checkPass(email, pass))) {
+            await new Promise((r) => setTimeout(r, 2000)); // This does not stop parallel requests from being sent. A more secure method might be an account-wide retry counter but this implementation was not covered in the scope of the class
+            return done(null, false, { 'message' : 'Incorrect password' });
         }
-        // success!
-        // should create a user object here, associated with a unique identifier
-        return done(null, username);
+        return done(null, email);
     });
 
 // app configurations
-app.use(express.json()); // lets you handle JSON input
 app.use(express.static('client/')); // specify the directory
-app.use(express.urlencoded({ 'extended': true })); // allow URLencoded data
-app.use(expressSession(session));
-passport.use(strategy);
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
+app.use(express.json()); // lets you handle JSON input
 
 // convert user object to a unique identifier.
 passport.serializeUser((user, done) => {
@@ -59,6 +56,35 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser((uid, done) => {
     done(null, uid);
 });
+
+app.use(expressSession(session));
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ 'extended': true }));
+
+const mustBeAuthenticated = async(req, res, next) => req.isAuthenticated() ? next() : res.redirect('/login');
+
+const userExists = async email => await getSaltHashOf(email) !== undefined;
+
+const userInfo = async email => {
+    if(!(await userExists(email))) {
+        return undefined;
+    }
+    const saltHash = await getSaltHashOf(email);
+    return {
+        salt: saltHash[0],
+        hash: saltHash[1]
+    };
+};
+
+const checkPass = async (email, pass) => {
+    const credInfo = await userInfo(email);
+    if(credInfo === undefined) {
+        return false;
+    }
+    return miniCrypt.check(pass, credInfo.salt, credInfo.hash);
+};
 
 // connect HTML frontend to server backend 
 app.get('/', (req, res) => {
@@ -83,12 +109,12 @@ app.post("/deleteUser", async (req, res) => {
     await dblast.delUser(data.email);
 });
 
-app.post('/getRoomById', async (req, res) => {
+app.post('/getRoomById', async (req,res) => {
     const data = req.body;
     res.send(JSON.stringify(await dblast.getRoomID(data.building)));
 });
 
-app.post('/editInfo', async (req, res) => {
+app.post('/editInfo', async (req,res) => {
     const data = req.body;
     await dblast.updateUserFirstName(data.firstname, data.password);
     await dblast.updateUserLastName(data.lastname, data.password);
@@ -99,43 +125,12 @@ app.get('/deleteProfile', (req, res) => {
     console.log("deleted user profile!");
 });
 
-// Handle post data from the login.html form.
-// TO DO: probably need to redirect differently
-app.post('/login',
-    passport.authenticate('local', {     // use username/password authentication
-        'successRedirect': '/userProfile',
-        'failureRedirect': '/login'      // otherwise, back to login
-    }));
-
-/*
-    // browser url http://localhost:3000/login 
-app.get('/login', async (req, res) => {
-    const body = req.body;
-    const user = await dblast.getUserByEmail({ email: body.email });
-    console.log(user.password);
-    if (user) {
-        const valid = await bcrypt.compare(body.password, user.password);
-        if (valid) {
-            console.log("Login Succeeded!");
-            res.sendFile(path.resolve('./client/userProfile.html',
-                { 'root': __dirname }));
-        } else {
-            res.status(400).json({ message: "Invalid password" });
-        }
-    } else {
-        res.status(401).json({ message: "User doesn't exist" });
-    }
-});
-*/
-
-app.get('/login', (req, res) => {
+// browser url http://localhost:3000/login
+app.get('/login', mustBeAuthenticated, (req, res) => {
     console.log("Login Succeeded!");
-    res.sendFile(path.resolve('./client/userProfile.html',
-        { 'root': __dirname }));
+    res.sendFile(path.resolve('./client/userProfile.html'));
 });
 
-
-// handle logging out
 app.get('/logout', (req, res) => {
     req.logout();
     res.redirect('/login');
@@ -144,29 +139,35 @@ app.get('/logout', (req, res) => {
 // curl -d '{ "email" : "x", "password" : "X", "firstName" : "x", "lastName" : "x", "userId" : "7", "groups" : ["Esports club"], "previousBookings" : [1], "upcomingBookings" : [2]}' -H "Content-Type: application/json" http://localhost:3000/createAccount
 app.post('/createAccount', async (req, res) => {
     const data = req.body;
-    let errors = [];
-    /*
-    if (!data.firstname || !data.lastname || !data.email || !data.password || !data.validatePassword) {
-        errors.push({ msg: "Please fill in all fields" });
-    } 
-    if (data.password !== data.validatePassword) {
-        errors.push({ msg: "Passwords do not match"});
+    console.log("password input: " + data.password);
+    console.log("hash: " + mc.hash(data.password));
+    const [salt, hash] = mc.hash(data.password);
+    await dblast.addUser(data.firstname, data.lastname, data.email, [salt, hash], data.previousbookings, data.upcomingbookings);
+    console.log(`Created new account successfully!`);
+});
+
+// browser url http://localhost:3000/userInfo?userId=1
+app.post('/userInfo', async (req, res) => {
+    const data = req.body;
+    res.send(JSON.stringify(await dblast.getUserByEmail(data.email)));
+});
+
+// https://www.codegrepper.com/code-examples/javascript/app.delete%28%29+express
+// browser url http://localhost:3000/deleteAccount?userId=1
+app.delete('/users/:id', (req, res, next) => {
+    const expressionIndex = getIndexById(req.params.id, expressions);
+    if (expressionIndex !== -1) {
+      expressions.splice(expressionIndex, 1);
+      res.status(204).send();
+    } else {
+      res.status(404).send();
     }
-    if (errors.length > 0) {
-        console.log(JSON.stringify(errors));
-        res.redirect('/');
-    }
-    else 
-    */
-        const hashed = await bcrypt.hash(data.password, 10);
-        await dblast.addUser(
-            data.firstname,
-            data.lastname,
-            data.email,
-            hashed
-        );
-        console.log("Account created!");
-        res.redirect('/')
+  });
+
+  // curl -d '{ "email" : "x", "password" : "X", "firstName" : "x", "lastName" : "x", "userId" : "5", "groups" : ["Esports club"], "previousBookings" : [1], "upcomingBookings" : [2]}' -H "Content-Type: application/json" http://localhost:3000/createAccount
+app.post('/deleteAccount', (req, res) => {
+    data["users"].pop(req.body.user);
+    console.log(`Deleted account successfully!`);
 });
 
 app.post('/createBooking', async (req, res) => {
@@ -175,35 +176,13 @@ app.post('/createBooking', async (req, res) => {
     console.log(`Created new booking successfully!`);
 });
 
-app.post('/userInfo', async (req, res) => {
-    const data = req.body;
-    res.send(JSON.stringify(await dblast.getUserByEmail(data.email)));
-});
-
-// https://www.codegrepper.com/code-examples/javascript/app.delete%28%29+express
-app.delete('/users/:id', (req, res, next) => {
-    const expressionIndex = getIndexById(req.params.id, expressions);
-    if (expressionIndex !== -1) {
-        expressions.splice(expressionIndex, 1);
-        res.status(204).send();
-    } else {
-        res.status(404).send();
-    }
-});
-
-// curl -d '{ "email" : "x", "password" : "X", "firstName" : "x", "lastName" : "x", "userId" : "5", "groups" : ["Esports club"], "previousBookings" : [1], "upcomingBookings" : [2]}' -H "Content-Type: application/json" http://localhost:3000/createAccount
-app.post('/deleteAccount', (req, res) => {
-    data["users"].pop(req.body.user);
-    console.log(`Deleted account successfully!`);
-});
-
 // Gather Room Information 
 app.post('/roomInformation', async (req, res) => {
-    const data = req.body;
+    const data = req.body;    
     res.send(JSON.stringify(await dblast.getRoomInformation(data.roomid)));
 });
 
-app.get('/allRooms', async (req, res) => {
+app.get('/allRooms', async (req,res) => {
     res.send(JSON.stringify(await dblast.getAllRooms()));
 })
 
@@ -240,7 +219,7 @@ app.get('/getTechRooms', async (req, res) => {
 
 app.get('*', (req, res) => {
     res.send('NO FOOL, BAD COMMAND');
-});
+  });
 
 const port = 3000; // specify the port 
 app.listen(process.env.PORT || port);
